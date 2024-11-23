9dcbfbb7f463f55ed2063450d98bbf1a764eebc1
shivaji <108468912+shivaji1134@users.noreply.github.com>
2024-11-24 02:09:19 +0530

    about and video audio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import NavBar from "./components/Navbar";
 
 const App = () => {
     return (
         <main className="relative min-h-screen w-screen overflow-x-hidden">
+            <NavBar />
             <Hero />
             <About />
         </main>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,62 @@
-import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
 
-const About = () =>{
-    return(
-        <div id='about' className="min-h-screen w-screen">
-            <div>
-                
-            </div>
+import AnimatedTitle from "./AnimatedTitle";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const About = () => {
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: 0,
+    });
+  });
+
+  return (
+    <div id="about" className="min-h-screen w-screen">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to Valorant
+        </p>
+
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the world's <br /> most popul<b>a</b>r shooter fps"
+          containerClass="mt-5 !text-black text-center"
+        />
+
+        <div className="about-subtext">
+          <p>The Game of Games begins—your life, with Epic Maps</p>
+          <p className="text-gray-500">
+            Valorant unites every player from countless games and platforms
+          </p>
         </div>
-    )
-}
+      </div>
 
-export default About
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/icebox.webp"
+            alt="Background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
