5ca9f1715a7fecd11efddbedf76b6aaa2c5b7963
shivaji <108468912+shivaji1134@users.noreply.github.com>
2024-11-24 16:58:56 +0530

    added about , footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import NavBar from "./components/Navbar";
+import Features from "./components/Features";
+import FloatingImage from "./components/Story";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
     return (
@@ -9,6 +13,10 @@ const App = () => {
             <NavBar />
             <Hero />
             <About />
+            <Features />
+            <FloatingImage />
+            <Contact />
+            <Footer />
         </main>
     )
 }
